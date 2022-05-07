73495a111b7492db82de7bb76de4630593c31021
qStivi <stephanglaue@gmail.com>
2022-05-07 18:37:00 +0200

    Last SortTools
    Signed-off-by: qStivi <stephanglaue@gmail.com>

--- Stivi/src/main/java/com/qStivi/SortTools.java
+++ Stivi/src/main/java/com/qStivi/SortTools.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class SortTools {
 
-    public static int NUMBER_OF_ITERATIONS = 10;
+    public static int NUMBER_OF_ITERATIONS = 1000;
 
 
     //region"create" functions
@@ -204,42 +204,178 @@ public class SortTools {
 
     //region merge-sort
 
-    private static void merge(int[] A, int p, int q, int r) {
-        var n1 = q - p + 1;
-        var n2 = r - q;
+    private static void merge(int[] A, int i, int j, int k) {
 
+        var n1 = (j - i) + 1;
+        var n2 = k - j;
+
+        // Create array for each side of merge (+1 for sentinel)
         var L = new int[n1 + 1];
         var R = new int[n2 + 1];
 
-        if (n1 - 1 >= 0) System.arraycopy(A, p + 1 - 1, L, 1, n1 - 1);
-        if (n2 - 1 >= 0) System.arraycopy(A, q + 1, R, 1, n2 - 1);
+        // fill both arrays with corresponding values
+        System.arraycopy(A, i, L, 0, n1);
+        System.arraycopy(A, j + 1, R, 0, n2);
 
-        L[n1 + 1] = Integer.MAX_VALUE;
-        L[n2 + 1] = Integer.MAX_VALUE;
+        // insert sentinels
+        L[n1] = Integer.MAX_VALUE;
+        R[n2] = Integer.MAX_VALUE;
 
-        var i = 1;
-        var j = 1;
-        for (int k = p; k < r; k++) {
-            if (L[i] <= R[j]) {
-                A[k] = L[i];
-                i++;
-            } else {
-                A[k] = R[j];
-                j++;
+        // create indices to keep track of position in each side
+        var l = 0;
+        var r = 0;
+
+        // Do the actual merging
+        for (int m = i; m <= k; m++) { // Go through the whole original array
+            if (L[l] <= R[r]) { // If current element of left array is smaller or equal than the current element of the right array
+                A[m] = L[l]; // Replace current element of original array with current element of left array
+                l++; // Make next element in left array the current element
+            } else { // Analog to left side (but "<" instead of "<=")
+                A[m] = R[r];
+                r++;
             }
         }
     }
 
-    public static void mergeSort(int[] A) {
-        mergeSortRec(A, A[A.length - 1], A[0]);
+    private static void merge(int[] A, int i, int j, int o, int k) {
+
+        var n1 = (j - i) + 1;
+        var n2 = o - j;
+        var n3 = k - o;
+
+        // Create array for each side of merge (+1 for sentinel)
+        var L = new int[n1 + 1];
+        var M = new int[n2 + 1];
+        var R = new int[n3 + 1];
+
+        // fill both arrays with corresponding values
+        System.arraycopy(A, i, L, 0, n1);
+        System.arraycopy(A, j + 1, M, 0, n2);
+        System.arraycopy(A, o + 1, R, 0, n3);
+
+        // insert sentinels
+        L[n1] = Integer.MAX_VALUE;
+        M[n2] = Integer.MAX_VALUE;
+        R[n3] = Integer.MAX_VALUE;
+
+        // create indices to keep track of position in each side
+        var l = 0;
+        var m = 0;
+        var r = 0;
+
+        // Do the actual merging
+        for (int p = i; p <= k; p++) {
+            if (L[l] <= M[m] && L[l] <= R[r]) {
+                A[p] = L[l];
+                l++;
+            } else if (M[m] <= R[r] && M[m] <= L[l]) {
+                A[p] = M[m];
+                m++;
+            } else {
+                A[p] = R[r];
+                r++;
+            }
+        }
     }
 
-    private static void mergeSortRec(int[] A, int p, int r) {
+    /**
+     * Sorts an array in ascending order using merge-sort<br>
+     * Worst-case performance: O(n*log n)<br>
+     * Best-case performance : Ω(n*log n) typical, Ω(n) natural variant<br>
+     * Average performance: Θ(n*log n)<br>
+     *
+     * @param A is the array which is to be sorted
+     */
+    public static void mergeSort(int[] A) {
+        mergeSortRec(A, 0, A.length - 1);
+    }
+
+    private static void mergeSortRec(int[] A, int i, int k) {
+        if (i < k) {
+            var j = (i + k) / 2;
+            mergeSortRec(A, i, j);
+            mergeSortRec(A, j + 1, k);
+            merge(A, i, j, k);
+        }
+    }
+
+    /**
+     * This is a variant of {@link SortTools#mergeSort(int[])} which spilts the array in 3 instead of 2
+     *
+     * @param A is the array which is to be sorted
+     * @see SortTools#mergeSort(int[])
+     */
+    public static void mergeSortNew(int[] A) {
+        SortTools.mergeSortNewRec(A, 0, A.length - 1);
+    }
+
+    private static void mergeSortNewRec(int[] A, int i, int k) {
+        if (i < k) {
+            var x = Math.round((k - i + 1) / 3f);
+            var j = x - 1 + i;
+            var o = j + x;
+            mergeSortNewRec(A, i, j);
+            mergeSortNewRec(A, j + 1, o);
+            mergeSortNewRec(A, o + 1, k);
+            merge(A, i, j, o, k);
+        }
+    }
+
+
+    /**
+     * This is the generic version of {@link SortTools#mergeSort(int[])}
+     */
+    public static <T extends Comparable<T>> void mergeSortGen(T[] a) {
+        mergeSortGenHelp(a, 0, a.length - 1);
+    }
+
+    public static <T extends Comparable<T>> void mergeSortGenHelp(T[] a, int p, int r) {
+
         if (p < r) {
-            var q = (p + r) / 2;
-            mergeSortRec(A, p, q);
-            mergeSortRec(A, q + 1, r);
-            merge(A, p, q, r);
+            int q = (int) Math.floor((p + r) / 2.0);
+            mergeSortGenHelp(a, p, q);
+            mergeSortGenHelp(a, q + 1, r);
+            mergeGen(a, p, q, r);
+        }
+
+    }
+
+    public static <T extends Comparable<T>> void mergeGen(T[] a, int p, int q, int r) {
+
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        // 0, 1, 2, 3, 4, 5, 6, 7, 8
+
+        var left = Arrays.copyOfRange(a, p, q + 1);
+        var right = Arrays.copyOfRange(a, q + 1, r + 1);
+
+        int i = 0;
+        int j = 0;
+
+        int k = p;
+
+        while (i < left.length && j < right.length && k <= r) {
+            if (left[i].compareTo(right[j]) <= 0) {
+                a[k] = left[i];
+                i++;
+            } else {
+                a[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < left.length) {
+            a[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.length) {
+            a[k] = right[j];
+            j++;
+            k++;
         }
     }
 
@@ -273,7 +409,7 @@ public class SortTools {
 
     //region time measurements
 
-    /*
+    /* Bubble Sort
     Average runtimes (NUMBER_OF_ITERATIONS = 10)
 
     n = 100:    136174
@@ -301,7 +437,7 @@ public class SortTools {
         return meanTime / NUMBER_OF_ITERATIONS; // divide total by number of iterations
     }
 
-    /*
+    /* Bubble Sort New
     Average runtimes (NUMBER_OF_ITERATIONS = 10)
 
     n = 100:    232666
@@ -330,14 +466,23 @@ public class SortTools {
     }
 
 
-    /*
-    Average runtimes (NUMBER_OF_ITERATIONS = 10)
+    /* Insertion sort
+    Average runtimes on an incremental sorted array (NUMBER_OF_ITERATIONS = 10)
 
-    n = 100:    69962
-    n = 1000:   723237
-    n = 10000:  11882270
-    n = 100000: 1071934204
-    n = 200000: 4252841279
+    n = 100:    1053
+    n = 1000:   3164
+    n = 10000:  2182
+    n = 100000: 20694
+    n = 200000: 41401
+
+
+    Average runtimes on a decremental sorted array (NUMBER_OF_ITERATIONS = 10)
+
+    n = 100:    5937
+    n = 1000:   329976
+    n = 10000:  32567395
+    n = 100000: 3292526041
+    n = 200000: idk :( war nach 3Stunden immer noch nicht fertig?!
      */
     public static long getMeanTimeInsertionSort(int[] array) {
         var meanTime = 0L;
@@ -358,40 +503,173 @@ public class SortTools {
         return meanTime / NUMBER_OF_ITERATIONS; // divide total by number of iterations
     }
 
+
+    /* Merge Sort
+    Average runtimes on an incremental sorted array (NUMBER_OF_ITERATIONS = 10)
+
+    n = 100:    4142
+    n = 1000:   27437
+    n = 10000:  283722
+    n = 100000: 2908530
+    n = 200000: 5858402
+
+
+    Average runtimes on a decremental sorted array (NUMBER_OF_ITERATIONS = 10)
+
+    n = 100:    2418
+    n = 1000:   27215
+    n = 10000:  249080
+    n = 100000: 2841072
+    n = 200000: 5774479
+     */
+    public static long getMeanTimeMergeSort(int[] array) {
+        var meanTime = 0L;
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) { // do it x times
+
+            var arr = Arrays.copyOf(array, array.length); // Create copy of array
+
+            var startTimer = System.nanoTime(); // Get start time
+
+            mergeSort(arr); // Sort array
+
+            var endTimer = System.nanoTime(); // Get end time
+
+            long diff = endTimer - startTimer; // Calculate runtime
+
+            meanTime += diff; // add difference to total
+        }
+        return meanTime / NUMBER_OF_ITERATIONS; // divide total by number of iterations
+    }
+
+
+    /* Merge Sort New
+    Average runtimes on an incremental sorted array (NUMBER_OF_ITERATIONS = 10)
+
+    n = 100:    3961
+    n = 1000:   27297
+    n = 10000:  259740
+    n = 100000: 2986923
+    n = 200000: 5655446
+
+
+    Average runtimes on a decremental sorted array (NUMBER_OF_ITERATIONS = 10)
+
+    n = 100:    1869
+    n = 1000:   22492
+    n = 10000:  253405
+    n = 100000: 2883243
+    n = 200000: 5393586
+     */
+    public static long getMeanTimeMergeSortNew(int[] array) {
+        var meanTime = 0L;
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) { // do it x times
+
+            var arr = Arrays.copyOf(array, array.length); // Create copy of array
+
+            var startTimer = System.nanoTime(); // Get start time
+
+            mergeSortNew(arr); // Sort array
+
+            var endTimer = System.nanoTime(); // Get end time
+
+            long diff = endTimer - startTimer; // Calculate runtime
+
+            meanTime += diff; // add difference to total
+        }
+        return meanTime / NUMBER_OF_ITERATIONS; // divide total by number of iterations
+    }
+
     //endregion
 
 
+    @SuppressWarnings("DuplicatedCode")
     public static void main(String[] args) {
         try {
             NUMBER_OF_ITERATIONS = Integer.parseInt(args[0]);
         } catch (Exception ignored) {
         }
 
-        var hundred = createSequenceDec(100);
-        var thousand = createSequenceDec(1000);
-        var tenThousand = createSequenceDec(10000);
-        var hundredThousand = createSequenceDec(100000);
-        var twoHundredThousand = createSequenceDec(200000);
+        var hundredDec = createSequenceDec(100);
+        var thousandDec = createSequenceDec(1000);
+        var tenThousandDec = createSequenceDec(10000);
+        var hundredThousandDec = createSequenceDec(100000);
+        var twoHundredThousandDec = createSequenceDec(200000);
 
-        System.out.println("InsertionSort");
-        System.out.println("100: " + getMeanTimeInsertionSort(hundred));
-        System.out.println("1.000: " + getMeanTimeInsertionSort(thousand));
-        System.out.println("10.000: " + getMeanTimeInsertionSort(tenThousand));
-        System.out.println("100.000: " + getMeanTimeInsertionSort(hundredThousand));
-        System.out.println("200.000: " + getMeanTimeInsertionSort(twoHundredThousand) + System.lineSeparator());
+        var hundredInc = createSequenceInc(100);
+        var thousandInc = createSequenceInc(1000);
+        var tenThousandInc = createSequenceInc(10000);
+        var hundredThousandInc = createSequenceInc(100000);
+        var twoHundredThousandInc = createSequenceInc(200000);
 
-        System.out.println("BubbleSort");
-        System.out.println("100: " + getMeanTimeBubbleSort(hundred));
-        System.out.println("1.000: " + getMeanTimeBubbleSort(thousand));
-        System.out.println("10.000: " + getMeanTimeBubbleSort(tenThousand));
-        System.out.println("100.000: " + getMeanTimeBubbleSort(hundredThousand));
-        System.out.println("200.000: " + getMeanTimeBubbleSort(twoHundredThousand) + System.lineSeparator());
+//        System.out.println("BubbleSortInc");
+//        System.out.println("100: " + getMeanTimeBubbleSort(hundredInc));
+//        System.out.println("1.000: " + getMeanTimeBubbleSort(thousandInc));
+//        System.out.println("10.000: " + getMeanTimeBubbleSort(tenThousandInc));
+//        System.out.println("100.000: " + getMeanTimeBubbleSort(hundredThousandInc));
+//        System.out.println("200.000: " + getMeanTimeBubbleSort(twoHundredThousandInc) + System.lineSeparator());
+//
+//        System.out.println("BubbleSortNewInc");
+//        System.out.println("100: " + getMeanTimeBubbleSortNew(hundredInc));
+//        System.out.println("1.000: " + getMeanTimeBubbleSortNew(thousandInc));
+//        System.out.println("10.000: " + getMeanTimeBubbleSortNew(tenThousandInc));
+//        System.out.println("100.000: " + getMeanTimeBubbleSortNew(hundredThousandInc));
+//        System.out.println("200.000: " + getMeanTimeBubbleSortNew(twoHundredThousandInc) + System.lineSeparator());
 
-        System.out.println("BubbleSortNew");
-        System.out.println("100: " + getMeanTimeBubbleSortNew(hundred));
-        System.out.println("1.000: " + getMeanTimeBubbleSortNew(thousand));
-        System.out.println("10.000: " + getMeanTimeBubbleSortNew(tenThousand));
-        System.out.println("100.000: " + getMeanTimeBubbleSortNew(hundredThousand));
-        System.out.println("200.000: " + getMeanTimeBubbleSortNew(twoHundredThousand) + System.lineSeparator());
+        System.out.println("MergeSortInc");
+        System.out.println("100: " + getMeanTimeMergeSort(hundredInc));
+        System.out.println("1.000: " + getMeanTimeMergeSort(thousandInc));
+        System.out.println("10.000: " + getMeanTimeMergeSort(tenThousandInc));
+        System.out.println("100.000: " + getMeanTimeMergeSort(hundredThousandInc));
+        System.out.println("200.000: " + getMeanTimeMergeSort(twoHundredThousandInc) + System.lineSeparator());
+
+        System.out.println("MergeSortNewInc");
+        System.out.println("100: " + getMeanTimeMergeSortNew(hundredInc));
+        System.out.println("1.000: " + getMeanTimeMergeSortNew(thousandInc));
+        System.out.println("10.000: " + getMeanTimeMergeSortNew(tenThousandInc));
+        System.out.println("100.000: " + getMeanTimeMergeSortNew(hundredThousandInc));
+        System.out.println("200.000: " + getMeanTimeMergeSortNew(twoHundredThousandInc) + System.lineSeparator() + System.lineSeparator());
+
+        System.out.println("InsertionSortInc");
+        System.out.println("100: " + getMeanTimeInsertionSort(hundredInc));
+        System.out.println("1.000: " + getMeanTimeInsertionSort(thousandInc));
+        System.out.println("10.000: " + getMeanTimeInsertionSort(tenThousandInc));
+        System.out.println("100.000: " + getMeanTimeInsertionSort(hundredThousandInc));
+        System.out.println("200.000: " + getMeanTimeInsertionSort(twoHundredThousandInc) + System.lineSeparator());
+
+
+//        System.out.println("BubbleSortDec");
+//        System.out.println("100: " + getMeanTimeBubbleSort(hundredDec));
+//        System.out.println("1.000: " + getMeanTimeBubbleSort(thousandDec));
+//        System.out.println("10.000: " + getMeanTimeBubbleSort(tenThousandDec));
+//        System.out.println("100.000: " + getMeanTimeBubbleSort(hundredThousandDec));
+//        System.out.println("200.000: " + getMeanTimeBubbleSort(twoHundredThousandDec) + System.lineSeparator());
+//
+//        System.out.println("BubbleSortNewDec");
+//        System.out.println("100: " + getMeanTimeBubbleSortNew(hundredDec));
+//        System.out.println("1.000: " + getMeanTimeBubbleSortNew(thousandDec));
+//        System.out.println("10.000: " + getMeanTimeBubbleSortNew(tenThousandDec));
+//        System.out.println("100.000: " + getMeanTimeBubbleSortNew(hundredThousandDec));
+//        System.out.println("200.000: " + getMeanTimeBubbleSortNew(twoHundredThousandDec) + System.lineSeparator());
+
+        System.out.println("MergeSortDec");
+        System.out.println("100: " + getMeanTimeMergeSort(hundredDec));
+        System.out.println("1.000: " + getMeanTimeMergeSort(thousandDec));
+        System.out.println("10.000: " + getMeanTimeMergeSort(tenThousandDec));
+        System.out.println("100.000: " + getMeanTimeMergeSort(hundredThousandDec));
+        System.out.println("200.000: " + getMeanTimeMergeSort(twoHundredThousandDec) + System.lineSeparator());
+
+        System.out.println("MergeSortNewDec");
+        System.out.println("100: " + getMeanTimeMergeSortNew(hundredDec));
+        System.out.println("1.000: " + getMeanTimeMergeSortNew(thousandDec));
+        System.out.println("10.000: " + getMeanTimeMergeSortNew(tenThousandDec));
+        System.out.println("100.000: " + getMeanTimeMergeSortNew(hundredThousandDec));
+        System.out.println("200.000: " + getMeanTimeMergeSortNew(twoHundredThousandDec) + System.lineSeparator());
+
+        System.out.println("InsertionSortDec");
+        System.out.println("100: " + getMeanTimeInsertionSort(hundredDec));
+        System.out.println("1.000: " + getMeanTimeInsertionSort(thousandDec));
+        System.out.println("10.000: " + getMeanTimeInsertionSort(tenThousandDec));
+        System.out.println("100.000: " + getMeanTimeInsertionSort(hundredThousandDec));
+        System.out.println("200.000: " + getMeanTimeInsertionSort(twoHundredThousandDec) + System.lineSeparator());
     }
 }
