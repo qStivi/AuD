d1af71cd7a5c062f655d948cd0f5f056f376d173
qStivi <stephanglaue@gmail.com>
2022-05-16 15:58:26 +0200

    PriorityQueue Beginnings
    Signed-off-by: qStivi <stephanglaue@gmail.com>

--- Stivi/src/main/java/com/qStivi/SearchTools.java
+++ Stivi/src/main/java/com/qStivi/SearchTools.java
@@ -17,13 +17,13 @@ public class SearchTools {
     }
 
     public static int binSearch(int[] A, int x, int l, int r) {
-        var i = (l + r) / 2;
-        if (r >= 1) {
+        if (l < r) {
+            var i = (l + r) / 2;
             if (x == A[i]) {
                 return i;
             } else if (x < A[i]) {
                 return binSearch(A, x, l, i);
-            } else if (x > A[i]) {
+            } else {
                 return binSearch(A, x, i + 1, r);
             }
         }
@@ -34,11 +34,13 @@ public class SearchTools {
     // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     //    l     i        j     r
     public static int binSearchNew(int[] A, int x, int l, int r) {
-        var i = ((r - l + 1) / 3) + l;
-        var j = i + (r - l + 1) / 3;
         if (r >= 1) {
+            var i = ((r - l + 1) / 3) + l;
+            var j = i + (r - l + 1) / 3;
             if (x == A[i]) {
                 return i;
+            } else if (x == A[j]) {
+                return j;
             } else if (x < A[i]) {
                 return binSearchNew(A, x, l, i);
             } else if (x > A[i] && x < A[j]) {
@@ -54,25 +56,50 @@ public class SearchTools {
     /*
     Die Werte sehen schon wieder so komisch aus :(
 
+    Random Number
+
     Linear Search
-        100.000: 37733
-      1.000.000: 275876
-    100.000.000: 29527009
-    685.154.321: 205591615
+        100.000: 19503
+      1.000.000: 104305
+    100.000.000: 10084235
+    685.154.321: 67621127
     _______________________
 
     Binary Search
-        100.000: 766
-      1.000.000: 1173
-    100.000.000: 15024
-    685.154.321: 2783 // warum geht das schneller als 100mio?!?!
+        100.000: 678
+      1.000.000: 923
+    100.000.000: 5424
+    685.154.321: 1478
     _______________________
 
     Binary Search New
-        100.000: 650
-      1.000.000: 548
-    100.000.000: 11925
-    685.154.321: 2459
+        100.000: 515
+      1.000.000: 488
+    100.000.000: 5379
+    685.154.321: 1408
+    _______________________
+
+    Worst Case
+
+    Linear Search
+        100.000: 23378
+      1.000.000: 182358
+    100.000.000: 18752872
+    685.154.321: 135180084
+    _______________________
+
+    Binary Search
+        100.000: 733
+      1.000.000: 181
+    100.000.000: 254
+    685.154.321: 190
+    _______________________
+
+    Binary Search New
+        100.000: 557
+      1.000.000: 209
+    100.000.000: 293
+    685.154.321: 188
     _______________________
     */
     public static void main(String[] args) {
@@ -81,64 +108,127 @@ public class SearchTools {
         var hundredMillion = SortTools.createSequenceInc(100000000);
         var sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne = SortTools.createSequenceInc(685154321);
 
+        System.out.println("Random Number");
+        System.out.println();
+
         System.out.println("Linear Search");
-        System.out.println("    100.000: " + getAverageLinerSearchTime(hundredThousand));
-        System.out.println("  1.000.000: " + getAverageLinerSearchTime(million));
-        System.out.println("100.000.000: " + getAverageLinerSearchTime(hundredMillion));
-        System.out.println("685.154.321: " + getAverageLinerSearchTime(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
+        System.out.println("    100.000: " + getAverageLinerSearchTimeRandom(hundredThousand));
+        System.out.println("  1.000.000: " + getAverageLinerSearchTimeRandom(million));
+        System.out.println("100.000.000: " + getAverageLinerSearchTimeRandom(hundredMillion));
+        System.out.println("685.154.321: " + getAverageLinerSearchTimeRandom(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
         System.out.println("_______________________");
         System.out.println();
 
         System.out.println("Binary Search");
-        System.out.println("    100.000: " + getAverageBinarySearchTime(hundredThousand));
-        System.out.println("  1.000.000: " + getAverageBinarySearchTime(million));
-        System.out.println("100.000.000: " + getAverageBinarySearchTime(hundredMillion));
-        System.out.println("685.154.321: " + getAverageBinarySearchTime(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
+        System.out.println("    100.000: " + getAverageBinarySearchTimeRandom(hundredThousand));
+        System.out.println("  1.000.000: " + getAverageBinarySearchTimeRandom(million));
+        System.out.println("100.000.000: " + getAverageBinarySearchTimeRandom(hundredMillion));
+        System.out.println("685.154.321: " + getAverageBinarySearchTimeRandom(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
         System.out.println("_______________________");
         System.out.println();
 
         System.out.println("Binary Search New");
-        System.out.println("    100.000: " + getAverageBinarySearchNewTime(hundredThousand));
-        System.out.println("  1.000.000: " + getAverageBinarySearchNewTime(million));
-        System.out.println("100.000.000: " + getAverageBinarySearchNewTime(hundredMillion));
-        System.out.println("685.154.321: " + getAverageBinarySearchNewTime(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
+        System.out.println("    100.000: " + getAverageBinarySearchNewTimeRandom(hundredThousand));
+        System.out.println("  1.000.000: " + getAverageBinarySearchNewTimeRandom(million));
+        System.out.println("100.000.000: " + getAverageBinarySearchNewTimeRandom(hundredMillion));
+        System.out.println("685.154.321: " + getAverageBinarySearchNewTimeRandom(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
+        System.out.println("_______________________");
+        System.out.println();
+
+        System.out.println("Worst Case");
+        System.out.println();
+
+        System.out.println("Linear Search");
+        System.out.println("    100.000: " + getAverageLinerSearchTimeWorst(hundredThousand));
+        System.out.println("  1.000.000: " + getAverageLinerSearchTimeWorst(million));
+        System.out.println("100.000.000: " + getAverageLinerSearchTimeWorst(hundredMillion));
+        System.out.println("685.154.321: " + getAverageLinerSearchTimeWorst(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
+        System.out.println("_______________________");
+        System.out.println();
+
+        System.out.println("Binary Search");
+        System.out.println("    100.000: " + getAverageBinarySearchTimeWorst(hundredThousand));
+        System.out.println("  1.000.000: " + getAverageBinarySearchTimeWorst(million));
+        System.out.println("100.000.000: " + getAverageBinarySearchTimeWorst(hundredMillion));
+        System.out.println("685.154.321: " + getAverageBinarySearchTimeWorst(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
+        System.out.println("_______________________");
+        System.out.println();
+
+        System.out.println("Binary Search New");
+        System.out.println("    100.000: " + getAverageBinarySearchNewTimeWorst(hundredThousand));
+        System.out.println("  1.000.000: " + getAverageBinarySearchNewTimeWorst(million));
+        System.out.println("100.000.000: " + getAverageBinarySearchNewTimeWorst(hundredMillion));
+        System.out.println("685.154.321: " + getAverageBinarySearchNewTimeWorst(sixHundredEightyFiveMillionOneHundredAndFiftyFourThousandAndThreeHundredAndTwentyOne));
         System.out.println("_______________________");
         System.out.println();
     }
 
-    private static long getAverageLinerSearchTime(int[] A) {
+    private static long getAverageLinerSearchTimeRandom(int[] A) {
         var avg = 0L;
         for (int i = 0; i < numberOfIterations; i++) {
             if (avg < 0) System.out.println("OVERFLOW!");
             var r = new Random().nextInt(1, A.length);
             var start = System.nanoTime();
             linSearch(A, r);
-            linSearch(A, -5);
             avg += System.nanoTime() - start;
         }
         return avg / numberOfIterations;
     }
 
-    private static long getAverageBinarySearchTime(int[] A) {
+    private static long getAverageBinarySearchTimeRandom(int[] A) {
         var avg = 0L;
         for (int i = 0; i < numberOfIterations; i++) {
             if (avg < 0) System.out.println("OVERFLOW!");
             var r = new Random().nextInt(1, A.length);
             var start = System.nanoTime();
             binSearch(A, r, 0, A.length - 1);
-            binSearch(A, -5, 0, A.length - 1);
             avg += System.nanoTime() - start;
         }
         return avg / numberOfIterations;
     }
 
-    private static long getAverageBinarySearchNewTime(int[] A) {
+    private static long getAverageBinarySearchNewTimeRandom(int[] A) {
         var avg = 0L;
         for (int i = 0; i < numberOfIterations; i++) {
             if (avg < 0) System.out.println("OVERFLOW!");
             var r = new Random().nextInt(1, A.length);
             var start = System.nanoTime();
             binSearchNew(A, r, 0, A.length - 1);
+            avg += System.nanoTime() - start;
+        }
+        return avg / numberOfIterations;
+    }
+
+    private static long getAverageLinerSearchTimeWorst(int[] A) {
+        var avg = 0L;
+        for (int i = 0; i < numberOfIterations; i++) {
+            if (avg < 0) System.out.println("OVERFLOW!");
+            var r = new Random().nextInt(1, A.length);
+            var start = System.nanoTime();
+            linSearch(A, -5);
+            avg += System.nanoTime() - start;
+        }
+        return avg / numberOfIterations;
+    }
+
+    private static long getAverageBinarySearchTimeWorst(int[] A) {
+        var avg = 0L;
+        for (int i = 0; i < numberOfIterations; i++) {
+            if (avg < 0) System.out.println("OVERFLOW!");
+            var r = new Random().nextInt(1, A.length);
+            var start = System.nanoTime();
+            binSearch(A, -5, 0, A.length - 1);
+            avg += System.nanoTime() - start;
+        }
+        return avg / numberOfIterations;
+    }
+
+    private static long getAverageBinarySearchNewTimeWorst(int[] A) {
+        var avg = 0L;
+        for (int i = 0; i < numberOfIterations; i++) {
+            if (avg < 0) System.out.println("OVERFLOW!");
+            var r = new Random().nextInt(1, A.length);
+            var start = System.nanoTime();
             binSearchNew(A, -5, 0, A.length - 1);
             avg += System.nanoTime() - start;
         }
