8b917e5fa587375adccf2d4c31049898df62e4d9
qStivi <stephanglaue@gmail.com>
2022-05-19 12:26:41 +0200

    a1
    Signed-off-by: qStivi <stephanglaue@gmail.com>

--- Stivi/src/main/java/com/qStivi/PriorityQueueMinHeap.java
+++ Stivi/src/main/java/com/qStivi/PriorityQueueMinHeap.java
@@ -1,37 +1,63 @@
 package com.qStivi;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 import static java.util.Collections.swap;
 
 public class PriorityQueueMinHeap<K extends Comparable<K>> implements PriorityQueue<K> {
 
-    private ArrayList<K> a;
+    private final ArrayList<K> a = new ArrayList<>();
+
+    // For testing purposes
+    public ArrayList<K> getA() {
+        return a;
+    }
 
     @Override
     public void addElement(K elem) {
+        if (a.size() == 0) {
+            a.add(0, elem);
+        } else {
+            a.add(0, elem);
+            minHeapify(a, 0);
+        }
+    }
 
+    // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    private void minHeapify(ArrayList<K> a, int i) {
+        var l = 2 * i + 1;
+        var r = 2 * i + 2;
+        int min;
+        if (l < a.size() && a.get(l).compareTo(a.get(i)) < 0) {
+            min = l;
+        } else {
+            min = i;
+        }
+        if (r < a.size() && a.get(r).compareTo(a.get(min)) < 0) {
+            min = r;
+        }
+        if (min != i) {
+            swap(a, i, min);
+            minHeapify(a, min);
+        }
     }
 
     @Override
     public K getFirst() {
-        return null;
+        if (a.size() < 1) {
+            throw new NoSuchElementException();
+        }
+        return a.get(0);
     }
 
     @Override
     public void deleteFirst() {
-
+        if (a.size() < 1) {
+            throw new NoSuchElementException();
+        }
+        a.remove(0);
+        minHeapify(a, 0);
     }
 
-    private void increaseKey(ArrayList<K> A, int i, K key) throws Exception {
-        if (key.compareTo(A.get(i)) < 0) {
-            throw new Exception("ERROR");
-        }
-        A.add(i, key);
-        var vater = i / 2;
-        while (i > 0 && A.get(vater).compareTo(A.get(i)) < 0) {
-            swap(A, i, vater);
-            i = vater;
-        }
-    }
 }
---
+++ Stivi/src/test/java/com/qStivi/PriorityQueueMinHeapTest.java
@@ -0,0 +1,70 @@
+package com.qStivi;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+class PriorityQueueMinHeapTest {
+
+    @Test
+    void addElement() {
+        var queue = new PriorityQueueMinHeap<Integer>();
+        queue.addElement(6); // 6
+        assertEquals(6, queue.getA().get(0));
+        queue.addElement(8); // 6, 8
+        assertEquals(8, queue.getA().get(1));
+        queue.addElement(4); // 4, 6, 8
+        assertEquals(4, queue.getA().get(0));
+        queue.addElement(Integer.MAX_VALUE); // 4, 6, 8, max
+        assertEquals(Integer.MAX_VALUE, queue.getA().get(3));
+        queue.addElement(Integer.MIN_VALUE);// min, 4, 6, 8, max
+        assertEquals(Integer.MIN_VALUE, queue.getA().get(0));
+        queue.addElement(17); // min, 4, 6, 8, 17, max
+        assertEquals(17, queue.getA().get(4));
+    }
+
+    @Test
+    void getFirst() {
+        var queue = new PriorityQueueMinHeap<Integer>();
+        assertThrowsExactly(NoSuchElementException.class, queue::getFirst);
+        queue.addElement(6); // 6
+        assertEquals(6, queue.getFirst());
+        queue.addElement(8); // 6, 8
+        assertEquals(6, queue.getFirst());
+        queue.addElement(4); // 4, 6, 8
+        assertEquals(4, queue.getFirst());
+        queue.addElement(Integer.MAX_VALUE); // 4, 6, 8, max
+        assertEquals(4, queue.getFirst());
+        queue.addElement(Integer.MIN_VALUE);// min, 4, 6, 8, max
+        assertEquals(Integer.MIN_VALUE, queue.getFirst());
+        queue.addElement(17); // min, 4, 6, 8, 17, max
+        assertEquals(Integer.MIN_VALUE, queue.getFirst());
+    }
+
+    @Test
+    void deleteFirst() {
+        var queue = new PriorityQueueMinHeap<Integer>();
+        queue.addElement(6); // 6
+        queue.addElement(8); // 6, 8
+        queue.addElement(4); // 4, 6, 8
+        queue.addElement(Integer.MAX_VALUE); // 4, 6, 8, max
+        queue.addElement(Integer.MIN_VALUE);// min, 4, 6, 8, max
+        queue.addElement(17); // min, 4, 6, 8, 17, max
+
+        queue.deleteFirst();
+        assertEquals(4, queue.getFirst());
+        queue.deleteFirst();
+        assertEquals(6, queue.getFirst());
+        queue.deleteFirst();
+        assertEquals(8, queue.getFirst());
+        queue.deleteFirst();
+        assertEquals(17, queue.getFirst());
+        queue.deleteFirst();
+        assertEquals(Integer.MAX_VALUE, queue.getFirst());
+        queue.deleteFirst();
+        assertThrowsExactly(NoSuchElementException.class, queue::deleteFirst);
+    }
+}
