dc7f4fcbb4e239145c558b7a51fb3bcb0a6cb434
qStivi <stephanglaue@gmail.com>
2022-06-11 15:23:12 +0200

    Done!
    Signed-off-by: qStivi <stephanglaue@gmail.com>

---
+++ Stivi/src/main/java/com/qStivi/HashmapForStrings.java
@@ -0,0 +1,117 @@
+package com.qStivi;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HashmapForStrings {
+
+    ArrayList<String>[] hashMap;
+
+    public HashmapForStrings(int m) {
+        this.hashMap = new ArrayList[m];
+        Arrays.setAll(hashMap, i -> new ArrayList<>());
+    }
+
+    /*
+    Insert size thousand:       60109917
+    Insert size ten thousand:   32325959
+    Number of Words not found:  1814
+    Search size thousand:       1294388333
+    Number of Words not found:  1814
+    Search size ten thousand:   4829617291
+     */
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        var thousand = new HashmapForStrings(1000);
+        var tenThousand = new HashmapForStrings(10000);
+
+        var classloader = Thread.currentThread().getContextClassLoader();
+        var wordListURL = classloader.getResource("OfficialScrabbleWordListGerman.txt");
+        var cheatingURL = classloader.getResource("AreMyFriendsCheating.txt");
+        if (wordListURL == null || cheatingURL == null) return;
+        var wordList = new File(wordListURL.toURI());
+        var cheating = new File(cheatingURL.toURI());
+
+        try (BufferedReader br = new BufferedReader(new FileReader(wordList))) {
+            var start = System.nanoTime();
+            for (String line; (line = br.readLine()) != null; ) {
+                thousand.insert(line);
+            }
+            System.out.println("Insert size thousand: " + (System.nanoTime() - start));
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(wordList))) {
+            var start = System.nanoTime();
+            for (String line; (line = br.readLine()) != null; ) {
+                tenThousand.insert(line);
+            }
+            System.out.println("Insert size ten thousand: " + (System.nanoTime() - start));
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(cheating))) {
+            var start = System.nanoTime();
+            var count = 0L;
+            for (String line; (line = br.readLine()) != null; ) {
+                if (!thousand.search(line)) {
+                    count++;
+                }
+            }
+            System.out.println("Number of Words not found: " + count);
+
+            System.out.println("Search size thousand: " + (System.nanoTime() - start));
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(cheating))) {
+            var start = System.nanoTime();
+            var count = 0L;
+            for (String line; (line = br.readLine()) != null; ) {
+                if (!tenThousand.search(line)) {
+                    count++;
+                }
+            }
+            System.out.println("Number of Words not found: " + count);
+
+            System.out.println("Search size ten thousand: " + (System.nanoTime() - start));
+        }
+
+
+    }
+
+    public long stringToLong(String string) {
+        long reduce = string.chars().asLongStream().reduce(0, Long::sum);
+        return reduce;
+    }
+
+    public int h(long n) {
+        int i = Math.floorMod(n, hashMap.length);
+        return i;
+    }
+
+    public void insert(String string) {
+        var stl = stringToLong(string);
+        var h = h(stl);
+        hashMap[h].add(string);
+    }
+
+    public boolean search(String string) {
+        for (ArrayList<String> strings : hashMap) {
+            for (String s : strings) {
+                if (s.equals(string)) return true;
+            }
+        }
+        return false;
+    }
+
+    public void delete(String string) {
+        for (ArrayList<String> strings : hashMap) {
+            for (int i = 0; i < strings.size(); i++) {
+                if (strings.get(i).equals(string)) {
+                    strings.remove(i);
+                    return;
+                }
+            }
+        }
+    }
+}
