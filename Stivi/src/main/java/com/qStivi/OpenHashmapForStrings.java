9756eb909c7d502931516b5fa2d546f9c4f67f30
qStivi <stephanglaue@gmail.com>
2022-06-30 16:57:24 +0200

    UndirectedGraph finished
    Signed-off-by: qStivi <stephanglaue@gmail.com>

--- Stivi/src/main/java/com/qStivi/OpenHashmapForStrings.java
+++ Stivi/src/main/java/com/qStivi/OpenHashmapForStrings.java
@@ -1,126 +1,111 @@
 package com.qStivi;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.NoSuchElementException;
 
-@SuppressWarnings("all") // :(
 public class OpenHashmapForStrings {
 
-    private final static String nulls = "nulls";
-    private final static String deleteds = "deleteds";
-    private static final ArrayList<String> OfficialScrabbleWordListGerman = new ArrayList<>();
-    private static final ArrayList<String> AreMyFriendsCheating = new ArrayList<>();
+    public static OpenHashmapForStrings linearProbing200000 = new OpenHashmapForStrings(200000, "Linear Probing");
+    public static OpenHashmapForStrings linearProbing1000000 = new OpenHashmapForStrings(1000000, "Linear Probing");
+    public static OpenHashmapForStrings quadraticProbing200000 = new OpenHashmapForStrings(200000, "Quadratic Probing");
+    public static OpenHashmapForStrings quadraticProbing1000000 = new OpenHashmapForStrings(1000000, "Quadratic Probing");
+    public static OpenHashmapForStrings doubleHashing200000 = new OpenHashmapForStrings(200000, "Double Hashing");
+    public static OpenHashmapForStrings doubleHashing1000000 = new OpenHashmapForStrings(1000000, "Double Hashing");
+    String[] hashtable;
+    boolean[] deleted;
+    boolean useLinearProbing = false;
+    boolean useQuadraticProbing = false;
+    boolean useDoubleHashing = false;
 
-    String[] hashMap;
+    public OpenHashmapForStrings(int size, String handleConflicts) {
+        hashtable = new String[size];
+        deleted = new boolean[size];
 
-    private OpenHashmapForStrings(int size) throws URISyntaxException, IOException {
-
-        var classloader = Thread.currentThread().getContextClassLoader();
-        var wordListURL = classloader.getResource("OfficialScrabbleWordListGerman.txt");
-        var cheatingURL = classloader.getResource("AreMyFriendsCheating.txt");
-        if (wordListURL == null || cheatingURL == null) return;
-        try (Stream<String> stream = Files.lines(Paths.get(wordListURL.toURI()))) {
-            stream.forEach(OfficialScrabbleWordListGerman::add);
+        if (handleConflicts.equalsIgnoreCase("Linear Probing")) {
+            useLinearProbing = true;
+        } else if (handleConflicts.equalsIgnoreCase("Quadratic Probing")) {
+            useQuadraticProbing = true;
+        } else if (handleConflicts.equalsIgnoreCase("Double Hashing")) {
+            useDoubleHashing = true;
         }
-        try (Stream<String> stream = Files.lines(Paths.get(cheatingURL.toURI()))) {
-            stream.forEach(AreMyFriendsCheating::add);
-        }
-
-        hashMap = new String[size];
-        Arrays.fill(hashMap, nulls);
     }
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
-        var million = new OpenHashmapForStrings(2000000);
-        var twoHundred = new OpenHashmapForStrings(200000000);
+    public static void initScrabbleWordMaps(File file) throws IOException {
 
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String word;
 
-        System.out.println("#####################");
-        System.out.println("200000 doubleInsert");
-        var a = new OpenHashmapForStrings(200000);
-        var time6 = new StopWatch();
-        try {
-            OfficialScrabbleWordListGerman.forEach(a::doubleInsert);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
+        while ((word = br.readLine()) != null) {
+
+//            System.out.println(word);
+
+            linearProbing200000.insert(word);
+//            System.out.println("Linear Probing 200000 OK");
+//            linearProbing1000000.insert(word);
+//            System.out.println("Linear Probing 1000000 OK");
+//            quadraticProbing200000.insert(word);
+//            System.out.println("Quadratic Probing 200000 OK");
+//            quadraticProbing1000000.insert(word);
+//            System.out.println("Quadratic Probing 1000000 OK");
+//            doubleHashing200000.insert(word);
+//            System.out.println("Double Hashing 200000 OK");
+//            doubleHashing1000000.insert(word);
+//            System.out.println("Double Hashing 1000000 OK");
         }
-        System.out.println(time6.getTime() + "ns");
-        System.out.println("#####################");
-
-        // ################################################################
-
-        System.out.println("200000 quadraticinsert");
-        var b = new OpenHashmapForStrings(200000);
-        var time5 = new StopWatch();
-        try {
-            OfficialScrabbleWordListGerman.forEach(b::quadraticInsert);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println(time5.getTime() + "ns");
-        System.out.println("#####################");
-
-        // ################################################################
-
-        System.out.println("200000 linearInsert");
-        var c = new OpenHashmapForStrings(200000);
-        var time4 = new StopWatch();
-        try {
-            OfficialScrabbleWordListGerman.forEach(c::linearInsert);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println(time4.getTime() + "ns");
-        System.out.println("#####################");
-
-        // ################################################################
-
-        System.out.println("1000000 doubleInsert");
-        var d = new OpenHashmapForStrings(1000000);
-        var time3 = new StopWatch();
-        try {
-            OfficialScrabbleWordListGerman.forEach(d::doubleInsert);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println(time3.getTime() + "ns");
-        System.out.println("#####################");
-
-        // ################################################################
-
-        System.out.println("1000000 quadraticinsert");
-        var e = new OpenHashmapForStrings(1000000);
-        var time2 = new StopWatch();
-        try {
-            OfficialScrabbleWordListGerman.forEach(e::quadraticInsert);
-        } catch (RuntimeException E) {
-            System.out.println(E.getMessage());
-        }
-        System.out.println(time2.getTime() + "ns");
-        System.out.println("#####################");
-
-        // ################################################################
-
-        System.out.println("1000000 linearInsert");
-        var f = new OpenHashmapForStrings(1000000);
-        var time1 = new StopWatch();
-        try {
-            OfficialScrabbleWordListGerman.forEach(f::linearInsert);
-        } catch (RuntimeException E) {
-            System.out.println(E.getMessage());
-        }
-        System.out.println(time1.getTime() + "ns");
-        System.out.println("#####################");
     }
 
-    // region stringtolongs
-    private long stringToLong(String string) {
-        char[] chars = string.toCharArray();
+    public static void searchWordsInScrabbleWordMaps(File file) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        int counterLinearProbing200000 = 0;
+        int counterLinearProbing1000000 = 0;
+        int counterQuadraticProbing200000 = 0;
+        int counterQuadraticProbing1000000 = 0;
+        int counterDoubleHashing200000 = 0;
+        int counterDoubleHashing1000000 = 0;
+        String word;
+
+        while ((word = br.readLine()) != null) {
+
+            int i1 = linearProbing200000.search(word);
+//            int i2 = linearProbing1000000.search(word);
+//            int i3 = quadraticProbing200000.search(word);
+//            int i4 = quadraticProbing1000000.search(word);
+//            int i5 = doubleHashing200000.search(word);
+//            int i6 = doubleHashing1000000.search(word);
+
+            if (i1 == -1) {
+                counterLinearProbing200000++;
+            }
+//            if(i2 == -1) {
+//                counterLinearProbing1000000++;
+//            }
+//            if(i3 == -1) {
+//                counterQuadraticProbing200000++;
+//            }
+//            if(i4 == -1) {
+//                counterQuadraticProbing1000000++;
+//            }
+//            if(i5 == -1) {
+//                counterDoubleHashing200000++;
+//            }
+//            if(i6 == -1) {
+//                counterDoubleHashing1000000++;
+//            }
+
+        }
+        System.out.println("Linear Probing: " + counterLinearProbing200000 + "; " + counterLinearProbing1000000);
+        System.out.println("Quadratic Probing: " + counterQuadraticProbing200000 + "; " + counterQuadraticProbing1000000);
+        System.out.println("Double Hashing: " + counterDoubleHashing200000 + "; " + counterDoubleHashing1000000);
+    }
+
+    private static long stringToValue(String element) {
+        char[] chars = element.toCharArray();
 
         long value = 1;
         int exponent = chars.length - 1;
@@ -133,105 +118,98 @@ public class OpenHashmapForStrings {
         return value;
     }
 
-    private long stringToLong2(String string) {
-        return string.chars().asLongStream().reduce(0, Long::sum);
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        var classloader = Thread.currentThread().getContextClassLoader();
+        var wordListURL = classloader.getResource("OfficialScrabbleWordListGerman.txt");
+        var cheatingURL = classloader.getResource("AreMyFriendsCheating.txt");
+        if (wordListURL == null || cheatingURL == null) return;
+        var wordList = new File(wordListURL.toURI());
+        var cheating = new File(cheatingURL.toURI());
+
+        initScrabbleWordMaps(wordList);
+        searchWordsInScrabbleWordMaps(cheating);
+
     }
 
-    private long stringToLong3(String string) {
-        return string.length();
+    private int helperFunction(String key) {
+        return (int) (stringToValue(key) % (long) hashtable.length);
     }
 
-    private long stringToLong4(String string) {
-        return string.hashCode();
+    private int helperFunction2(String key) {
+
+        //System.out.println("HelperFunction2: " + (1 + (int) (stringToValue(key) % (long) (hashtable.length - 1))));
+
+        return (int) (1 + (stringToValue(key) % ((long) hashtable.length - 1)));
     }
 
-    // endregion
-
-    // region inserts
-    public int linearInsert(String string) {
-        var i = 0;
-        while (i < hashMap.length) {
-            var h = (int) ((h(string) + i) % (long) hashMap.length);
-            if (hashMap[h] == nulls || hashMap[h] == deleteds) {
-                hashMap[h] = string;
-                return h;
-            } else {
-                i++;
-            }
-        }
-        var count = 0;
-        for (String s : hashMap) {
-            if (s == nulls) count++;
-        }
-        System.out.println("Still free: " + count);
-        throw new RuntimeException("HashMap full!");
+    private int linearProbing(String key, int i) {
+        return (int) ((helperFunction(key) + i) % (long) hashtable.length);
     }
 
-    public int quadraticInsert(String string) {
-        var i = 0;
-        while (i < hashMap.length) {
-            var h = (int) (h(string) + ((1d / 2d) * i) + ((1d / 2d) * Math.pow(i, 2))) % hashMap.length;
-            if (hashMap[h] == nulls || hashMap[h] == deleteds) {
-                hashMap[h] = string;
-                return h;
-            } else {
-                i++;
-            }
-        }
-        var count = 0;
-        for (String s : hashMap) {
-            if (s == nulls) count++;
-        }
-        System.out.println("Still free: " + count);
-        throw new RuntimeException("HashMap full!");
+    private int quadraticProbing(String key, int i) {
+        double c1 = 0.5;
+        double c2 = 0.5;
+        return (int) ((helperFunction(key) + c1 * i + c2 * i * i) % (long) hashtable.length);
     }
 
-    public int doubleInsert(String string) {
-        var i = 0;
-        while (i < hashMap.length) {
-            var e = h(string) + (i * h2(string));
-            var h = Math.floorMod(e, hashMap.length);
-            if (hashMap[h] == nulls || hashMap[h] == deleteds) {
-                hashMap[h] = string;
-                return h;
-            } else {
-                i++;
-            }
-        }
-        var count = 0;
-        for (String s : hashMap) {
-            if (s == nulls) count++;
-        }
-        System.out.println("Still free: " + count);
-        throw new RuntimeException("HashMap full!");
+    private int doubleHashing(String key, int i) {
+        return (int) ((helperFunction(key) + ((long) i * helperFunction2(key))) % (long) hashtable.length);
     }
 
-    // endregion
+    private int hash(String key, int i) {
+        if (useLinearProbing) {
+            return linearProbing(key, i);
+        } else if (useQuadraticProbing) {
+            return quadraticProbing(key, i);
+        } else {
+            return doubleHashing(key, i);
+        }
+    }
 
-    public Integer search(String string) {
-        var i = 0;
-        while (i < hashMap.length) {
-            var j = (h(string) + i) % hashMap.length;
-            if (hashMap[j] == null) {
-                return null;
-            }
-            if (hashMap[j].equals(string)) {
+    public int insert(String key) {
+        int i = 0;
+        while (i < hashtable.length) {
+            int j = hash(key, i);
+            if (hashtable[j] == null) {
+                hashtable[j] = key;
+                deleted[j] = false;
                 return j;
+            } else {
+                i = i + 1;
             }
-            i++;
         }
-        return null;
+        throw new IllegalStateException("Hashtable is full");
     }
 
-    public void delete(String string) {
-        throw new RuntimeException("Not implemented yet!");
+    public boolean delete(String key) {
+        int j = search(key);
+        if (hashtable[j] != null) {
+            deleted[j] = true;
+            hashtable[j] = null;
+            return true;
+        }
+        return false;
     }
 
-    public int h(String string) {
-        return (int) (stringToLong(string) % (long) hashMap.length);
+    public int search(String key) throws NoSuchElementException {
+        int i = 0;
+        while (i < hashtable.length) {
+            int j = hash(key, i);
+            if (hashtable[j] == null && !deleted[j]) { //Sobald einmal null und nicht gelöscht gefunden abbrechen
+                return -1;
+            } else if (hashtable[j] != null && hashtable[j].equals(key)) {
+                return j;
+            } else {
+                i = i + 1;
+            }
+        }
+        return -1;
     }
 
-    public int h2(String string) {
-        return 1 + Math.floorMod((stringToLong(string)), (hashMap.length - 1));
+    @Override
+    public String toString() {
+        return Arrays.toString(hashtable);
     }
+
+
 }
